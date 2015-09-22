3347dba3f967588d544b50c0753996c1e500bcd4
drinkingcoder <drinkingcoder@gmail.com>
2015-09-22 12:06:23 +0800

    Initial Commit

---
+++ drs03/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  drs03
+//
+//  Created by drinking on 9/22/15.
+//  Copyright (c) 2015 drinking. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
